2ea4312c7785d7f50ea2f3488861b9b29b5bc778
andralex8 <andralex8@gmail.com>
2017-09-28 09:31:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+Device Tree for LG Q6 m700n
+===========================================
+
+The LG Q6 is a low-mid range smartphone from LG with bezel-less 18/9 screen.
+
+Copyright 2017 - The LineageOS Project.
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Octo-core 1.4 GHz Cortex-A53 & Quad-core 1.1 GHz Cortex-A53
+Chipset | Qualcomm MSM8940 Snapdragon 435
+GPU     | Adreno 505
+Memory  | 3 GB RAM
+Shipped Android Version | 7.1.1
+Storage | 32 GB
+MicroSD | Up to 256GB
+Battery | Non-removable Li-Po 3000 mAh battery
+Display | 2160x1080 pixels, 5.5 inches, 18:9 ratio
+Camera  | Primary: 13 MP, f/2.2
+Camera (front)	| 5 MP, f/2.2
+
+Copyright (C) 2017 The LineageOS Project.
+
+![LG Q6](http://cdn2.gsmarena.com/vv/pics/lg/lg-q6-1.jpg "LG Q6")
---
+++ full_m700n.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from m700n device
+$(call inherit-product, device/lge/m700n/device.mk)
+
+# Set those variables here to overwrite the inherited values.
+BOARD_VENDOR := lge
+PRODUCT_DEVICE := m700n
+PRODUCT_NAME := lineage_m700n
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG Q6
+PRODUCT_MANUFACTURER := LGE
